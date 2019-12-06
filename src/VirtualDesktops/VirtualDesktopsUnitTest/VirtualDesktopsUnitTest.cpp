3b4044e00f8752a1936c2a61df97e6cf46ce7c35
John Sheehan <jsheehan@live.com>
2019-12-06 15:04:33 -0800

    Small updates to cookie tracking for events.

--- src/VirtualDesktops/VirtualDesktopsUnitTest/VirtualDesktopsUnitTest.cpp
+++ src/VirtualDesktops/VirtualDesktopsUnitTest/VirtualDesktopsUnitTest.cpp
@@ -14,28 +14,23 @@ namespace VirtualDesktopsUnitTest
 		{
 			VirtualDesktopManagerInternal vdmi;
 
-			uint64_t currentDesktopChangedCookie = vdmi.CurrentDesktopChanged.register_listener([](const VirtualDesktopManagerInternal& src, const VirtualDesktopChangedEventArgs& args) {
+			auto currentDesktopChangedCookie = vdmi.CurrentDesktopChanged.register_listener([](const VirtualDesktopManagerInternal& src, const VirtualDesktopChangedEventArgs& args) {
 				OutputDebugString(L"Current virtual desktop changed\n");
 			});
 
-			uint64_t desktopAddedCookie = vdmi.VirtualDesktopAdded.register_listener([](const VirtualDesktopManagerInternal& src, const VirtualDesktopAddedEventArgs& args) {
+			auto desktopAddedCookie = vdmi.VirtualDesktopAdded.register_listener([](const VirtualDesktopManagerInternal& src, const VirtualDesktopAddedEventArgs& args) {
 				OutputDebugString(L"Virtual desktop added\n");
-				});
+			});
 
-			uint64_t desktopRemovedCookie = vdmi.VirtualDesktopRemoved.register_listener([](const VirtualDesktopManagerInternal& src, const VirtualDesktopRemovedEventArgs& args) {
+			auto desktopRemovedCookie = vdmi.VirtualDesktopRemoved.register_listener([](const VirtualDesktopManagerInternal& src, const VirtualDesktopRemovedEventArgs& args) {
 				OutputDebugString(L"Virtual desktop removed\n");
-				});
+			});
 
-			uint64_t windowChangedDesktopCookie = vdmi.WindowChangedDesktops.register_listener([](const VirtualDesktopManagerInternal& src, const WindowChangedDesktopEventArgs& args) {
+			auto windowChangedDesktopCookie = vdmi.WindowChangedDesktops.register_listener([](const VirtualDesktopManagerInternal& src, const WindowChangedDesktopEventArgs& args) {
 				OutputDebugString(L"Window changed desktop\n");
-				});
+			});
 
-			Sleep(100000);
-
-			vdmi.CurrentDesktopChanged.unregister_listener(currentDesktopChangedCookie);
-			vdmi.VirtualDesktopAdded.unregister_listener(desktopAddedCookie);
-			vdmi.VirtualDesktopRemoved.unregister_listener(desktopRemovedCookie);
-			vdmi.WindowChangedDesktops.unregister_listener(windowChangedDesktopCookie);
+			Sleep(10000);
 		}
 	};
 }
